e892deea9fff9c542046f5ebef531099e13f07f2
Soumyaranjanp68 <soumyaranjanp68@gmail.com>
2023-05-30 16:16:29 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import the CSS file
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleIncrementTen = () => {
+    setCount(count + 10);
+  };
+
+  const handleDecrementTen = () => {
+    setCount(count - 10);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="sub-cont">
+      <div className="buttons">
+        
+        <button className="decrement" onClick={handleDecrement}>
+          -
+        </button>
+        <div className="count">
+        <button className="decrement-ten" onClick={handleDecrementTen}>
+          -10
+        </button>
+        Count: {count}
+        <button className="increment-ten" onClick={handleIncrementTen}>
+          +10
+        </button>
+        </div>
+        <button className="increment" onClick={handleIncrement}>
+          +
+        </button>
+        
+      </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
